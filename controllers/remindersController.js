b7836c4f290402d37340f6677b9caa44399e62f9
atakowiec <mat.cieszczyk@gmail.com>
2022-12-07 18:12:01 +0100

    Implement removeReminder and getReminders functions in remindersController.js

--- controllers/remindersController.js
+++ controllers/remindersController.js
@@ -1,17 +1,37 @@
 const usersDB = require("../model/UsersDB")
 
 const getReminders = (req, res) => {
-    const { profiles, deleted } = req.body;
+    const { profiles, active } = req.body;
     if(!profiles)
         return res.status(400).json({"message": "No profiles specified"})
     const username = req.user;
 
-    const remindersList = usersDB.getUserData(username).reminders
+    const everyReminders = usersDB.getUserData(username).reminders;
 
+    const resultReminders = everyReminders.map(
+            reminder => (reminder.profiles.some(profile => profiles.indexOf(profile) >= 0) &&
+            reminder.active === active)
+    );
+
+    return res.json(resultReminders).sendStatus(200);
 }
 
-const removeReminder = (req, res) => {
-    res.status(200).json({"user": req.user})
+const removeReminder = async (req, res) => {
+    const reminderId = req.body.reminderId;
+    if(!reminderId)
+        return res.sendStatus(204)
+    const userData = usersDB.getUserData(req.user)
+
+    userData.reminders = userData?.reminders.map(reminder => {
+        reminder.active = false
+        return reminder;
+    });
+
+    const otherUsers = usersDB.users.map(user => user.username !== req.user)
+
+    await usersDB.setUsers([...otherUsers, userData]).saveFile();
+
+    res.sendStatus(200)
 }
 
 const createReminder = (req, res) => {
